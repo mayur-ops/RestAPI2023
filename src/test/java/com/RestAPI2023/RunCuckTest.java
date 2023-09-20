c41ca03291905c978a653fce255c1e5a4e0d8a50
Mayurkumar patel <you@example.com>
2023-09-20 17:50:59 +0100

    run class created

---
+++ src/test/java/com/RestAPI2023/RunCuckTest.java
@@ -0,0 +1,13 @@
+package com.RestAPI2023;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "")
+
+public class RunCuckTest {
+
+}
